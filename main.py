e27584340e2b4846bb6f3d59909b22ab6b5edcbb
mohammadakhavan75 <mohammad.akhavan75@gmail.com>
2023-10-25 10:59:56 +0330

    working with one img

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from tflite_loader import model_loader
-import tflite_runtime.interpreter as tflite
+# import tflite_runtime.interpreter as mytflite
 import argparse
-from data_loader import data_loader
+import numpy as np
 
 def parsing():
-    parser = argparse.ArgumentParser(description='Tunes a CIFAR Classifier with OE',
+    parser = argparse.ArgumentParser(description='',
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--image_path', help='File path of image file', type=str, required=True)
-    parser.add_argument('--delegate_path', help='File path of ArmNN delegate file', type=str, defualt=None)
-    parser.add_argument('--preferred_backends', help='list of backends in order of preference', defualt=None, type=str, nargs='+', required=False, default=["CpuAcc", "CpuRef"]
-)
+    parser.add_argument('--img_path', help='File path of image file', type=str, required=True)
+    parser.add_argument('--model_path', help='Model path file', type=str, required=True)
+    parser.add_argument('--delegate_path', help='File path of ArmNN delegate file', type=str, default=None)
+    parser.add_argument('--preferred_backends', help='list of backends in order of preference', type=str, nargs='+', required=False, default=["CpuAcc", "CpuRef"])
     args = parser.parse_args()
+    args.armnn_delegate = None
     return args
 if __name__ == '__main__':
     args = parsing()
@@ -18,12 +19,11 @@ if __name__ == '__main__':
     # backends = args.preferred_backends
     # backends = ",".join(backends)
     # #load the delegate
-    # args.armnn_delegate = tflite.load_delegate(delegate_path,
+    # args.armnn_delegate = mytflite.load_delegate(delegate_path,
     # options={
     #     "backends": backends,
     #     "logging-severity": "info"})
     
     loader = model_loader(args)
-    data_loader = data_loader(args)
-    img = data_loader.load()
-    out = loader.inference(img)
+    out = loader.inference(args)
+    print(f"output is recived {out} \n shape is: {out.shape}, argmax: {np.argmax(out, axis=1)}")
--- tflite_loader.py
+++ tflite_loader.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import numpy as np
-import tflite_runtime.interpreter as tflite
+from data_loader import data_loader
+# import tflite_runtime.interpreter as tflite
 
 class model_loader():
     def __init__(self, args):
         # Load TFLite model and allocate tensors.
-        self.interpreter = tf.lite.Interpreter(model_path=args.model_path)#, experimental_delegates=[args.armnn_delegate])
+        if args.armnn_delegate is None:
+            self.interpreter = tf.lite.Interpreter(model_path=args.model_path)
+        else:
+            self.interpreter = tf.lite.Interpreter(model_path=args.model_path, experimental_delegates=[args.armnn_delegate])
         self.interpreter.allocate_tensors()
 
         # Get input and output tensors.
@@ -13,16 +17,22 @@ class model_loader():
         self.output_details = self.interpreter.get_output_details()
 
         # Test model on random input data.
-        self.input_shape = self.input_details[0]['shape']
+        self.input_shape = self.input_details[0]['shape'][2]
+        self.model_shape = self.input_details[0]['shape']
+        print("Expected input shape:", self.input_details[0]['shape'])
+
+    def _pre_processing(self, args):
+        loader = data_loader(args.img_path, img_shape=self.input_shape)
+        img = loader.load()
+        return img
 
 
-    def _pre_processing(self, img):
-        return tf.image.resize_images(img, (self.input_shape, self.input_shape))
-
-
-    def inference(self, img):
+    def inference(self, args):
         # BGR image to tensor
-        input_tensor = self._pre_processing(img)
+        input_tensor = self._pre_processing(args)
+        if input_tensor.shape != self.model_shape:
+            raise ValueError(f"Input data shape {input_tensor.shape} does not match the expected shape {self.model_shape}")
+
         self.interpreter.set_tensor(self.input_details[0]['index'], input_tensor)
         # set tensor and invoke
         self.interpreter.invoke()
