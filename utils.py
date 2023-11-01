9ba9f92479fa3522ba9ba74cf20c59c07df9fdda
mohammadakhavan75 <mohammad.akhavan75@gmail.com>
2023-11-01 10:57:53 +0330

    correcting coco parser to read from folder img and labels

--- utils.py
+++ utils.py
@@ -2,48 +2,33 @@ from collections import defaultdict
 import json
 import numpy as np
 from sklearn.metrics import confusion_matrix
+import os
 
 class COCOParser:
-    def __init__(self, anns_file, imgs_dir):
-        with open(anns_file, 'r') as f:
-            coco = json.load(f)
-            
-        self.annIm_dict = defaultdict(list)        
-        self.cat_dict = {} 
-        self.annId_dict = {}
-        self.im_dict = {}
-        self.licenses_dict = {}
+    def __init__(self, labels_root_path, imgs_root_path):
+        self.imgs_root_path = imgs_root_path
+        self.labels_root_path = labels_root_path
+        self.labels_dict = {}
+        self.imgs_dict = {}
 
-        for ann in coco['annotations']:           
-            self.annIm_dict[ann['image_id']].append(ann) 
-            self.annId_dict[ann['id']]=ann
-        for img in coco['images']:
-            self.im_dict[img['id']] = img
-        for cat in coco['categories']:
-            self.cat_dict[cat['id']] = cat
-        for license in coco['licenses']:
-            self.licenses_dict[license['id']] = license
+    def load_labels(self):
+        for filename in os.listdir(self.labels_root_path):
+            if filename.endswith('.txt'):
+                file_path = os.path.join(self.labels_root_path, filename)
+                
+                with open(file_path, 'r') as file:
+                    labels = file.read()
+                    self.labels_dict[filename.split('.')[0]] = labels.split('\n')[:-1]
 
-    def get_imgIds(self):
-        return list(self.im_dict.keys())
-
-    def get_annIds(self, im_ids):
-        im_ids=im_ids if isinstance(im_ids, list) else [im_ids]
-        return [ann['id'] for im_id in im_ids for ann in self.annIm_dict[im_id]]
-
-    def load_anns(self, ann_ids):
-        im_ids=ann_ids if isinstance(ann_ids, list) else [ann_ids]
-        return [self.annId_dict[ann_id] for ann_id in ann_ids]        
-
-    def load_id(self, class_ids):
-        class_ids=class_ids if isinstance(class_ids, list) else [class_ids]
-        return [self.cat_dict[class_id] for class_id in class_ids]
-
-    def get_imgLicenses(self,im_ids):
-        im_ids=im_ids if isinstance(im_ids, list) else [im_ids]
-        lic_ids = [self.im_dict[im_id]["license"] for im_id in im_ids]
-        return [self.licenses_dict[lic_id] for lic_id in lic_ids]
-
+        return self.labels_dict
+    
+    def load_img_paths(self):
+        for filename in os.listdir(self.imgs_root_path):
+            if filename.endswith('.jpg'):
+                file_path = os.path.join(self.imgs_root_path, filename)
+                self.imgs_dict[filename] = file_path
+        return self.imgs_dict
+    
 class metrics():
     def __init__(self, y_true, y_pred):
         self.c_m = confusion_matrix(y_true, y_pred)
